d895c489a79aecf508dd0cc44b347fa167e5a658
Nadav Ishai <nadavis@ac.sce.ac.il>
2022-04-13 20:29:31 +0300

    some bug fixes

--- DoggieSitter/accounts/urls.py
+++ DoggieSitter/accounts/urls.py
@@ -1,2 +1,11 @@
 # accounts/urls.py
 from django.urls import path
+from django.views.generic import TemplateView
+
+from . import views
+
+urlpatterns = [
+    path("signup/", views.SignUpView, name="signup"),
+    path('about', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('gallery', TemplateView.as_view(template_name='gallery.html'), name='gallery'),
+]
