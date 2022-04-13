d3ebfe63f2dbbe12b4f75980c2f7999fbc8b47f3
elinorel <73400268+elinorel@users.noreply.github.com>
2022-04-13 16:02:09 +0300

    BSPM2022T3-2
    BSPM2022T3-2

--- DoggieSitter/accounts/urls.py
+++ DoggieSitter/accounts/urls.py
@@ -1,2 +1,11 @@
 # accounts/urls.py
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+from . import views,templates
+
+urlpatterns = [
+    path("signup/", views.SignUpView, name="signup"),
+    path('about', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('gallery', TemplateView.as_view(template_name='gallery.html'), name='gallery'),
+]
