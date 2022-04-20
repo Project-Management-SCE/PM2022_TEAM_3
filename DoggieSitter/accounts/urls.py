707930587887f4ac948acab095bcb56d5a44947a
moranshalvi <73400663+moranshalvi@users.noreply.github.com>
2022-04-20 17:09:57 +0300

    BSPM2022T3-18
    The contact page is ready

--- DoggieSitter/accounts/urls.py
+++ DoggieSitter/accounts/urls.py
@@ -1,12 +1,12 @@
 # accounts/urls.py
 from django.urls import path, include
 from django.views.generic import TemplateView
-
 from . import views,templates
 
 urlpatterns = [
     path("signup/", views.SignUpView, name="signup"),
     path('about', TemplateView.as_view(template_name='about.html'), name='about'),
     path('gallery', TemplateView.as_view(template_name='gallery.html'), name='gallery'),
-    path('contact', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    path('about', TemplateView.as_view(template_name='about.html'), name='contact'),
+    path('update_details', TemplateView.as_view(template_name='update_details.html'), name='update_details'),
 ]
