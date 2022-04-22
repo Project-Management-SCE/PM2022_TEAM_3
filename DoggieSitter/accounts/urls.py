0a1592a8b963f2a6fa057cc1d7647b2bcb280af0
moranshalvi <73400663+moranshalvi@users.noreply.github.com>
2022-04-22 10:38:54 +0300

    BSPM2022T3-11
    Going to the appropriate pages works but the page is still in the process

--- DoggieSitter/accounts/urls.py
+++ DoggieSitter/accounts/urls.py
@@ -1,8 +1,10 @@
 # accounts/urls.py
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.views.generic import TemplateView
-
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 from . import views
+from .forms import MyPasswordChangeForm
+
 
 urlpatterns = [
     path("signup/", views.SignUpView, name="signup"),
@@ -10,4 +12,17 @@ urlpatterns = [
     path('gallery', TemplateView.as_view(template_name='gallery.html'), name='gallery'),
     path("user_info/", views.GetAccounts, name="user_info"),
     path('about', TemplateView.as_view(template_name='about.html'), name='contact'),
+    path('change_password/',
+        PasswordChangeView.as_view(
+            template_name='change_password.html',
+            success_url=reverse_lazy('password_change_done'),
+            form_class = MyPasswordChangeForm
+         ),
+         name='password_change'),
+
+    path('change_password/done/',
+        PasswordChangeDoneView.as_view(
+            template_name='password_change_done.html'
+        ),
+        name='password_change_done'),
 ]
