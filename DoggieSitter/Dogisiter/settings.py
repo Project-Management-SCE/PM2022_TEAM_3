808d7d2657687ae62abf7349041db1e5d67ea103
moranshalvi <73400663+moranshalvi@users.noreply.github.com>
2022-04-23 03:18:03 +0300

    BSPM2022T3-11
    done

--- DoggieSitter/Dogisiter/settings.py
+++ DoggieSitter/Dogisiter/settings.py
@@ -125,7 +125,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
-
-# django_project/settings.py
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
