75e6dbb35638e09adc26fb412031592b532013fe
BoazBitt <73490112+BoazBitt@users.noreply.github.com>
2022-04-29 11:22:15 +0300

    Update settings.py

--- DoggieSitter/Dogisiter/settings.py
+++ DoggieSitter/Dogisiter/settings.py
@@ -37,8 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'django_jenkins',
+
 ]
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+)
+PROJECT_APPS = (
+'accounts',
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
