097f1c6c3f70a2eeacdf64d43721145b5d7db0c9
BoazBitt <73490112+BoazBitt@users.noreply.github.com>
2022-04-29 18:34:49 +0300

    Update admin.py

--- DoggieSitter/accounts/admin.py
+++ DoggieSitter/accounts/admin.py
@@ -1,60 +1,60 @@
 from django.contrib import admin
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import Accounts
 from django.contrib.auth.models import User
 
-actions = ['make_published']
+# actions = ['make_published']
 
-admin.site.register(Accounts)
-
-
-@admin.action(description='Grant admin permissions')
-def make_new_admin(request):
-    user = User.objects.get(username=request.POST.get("selected_id"))
-    for i in Accounts.objects.all():
-        if str(i) == str(user):
-            ac = Accounts.objects.get(id=i.id)
-    ac.is_admin = True
-    user.is_superuser = True
-    user.is_staff = True
-    user.save()
-    ac.save()
-    return render(request, 'successful_action.html', {'result': "Admin permissions was successfully granted."})
-
-@admin.action(description='Disable admin permissions')
-def delete_admin(request):
-    user = User.objects.get(username=request.POST.get("selected_id"))
-    for i in Accounts.objects.all():
-        if str(i) == str(user):
-            ac = Accounts.objects.get(id=i.id)
-    ac.is_admin = False
-    user.is_superuser = False
-    user.is_staff = False
-    user.save()
-    ac.save()
-    return render(request, 'successful_action.html', {'result': "Admin permissions was successfully denied."})
-
-def delete_user(request):
-    user = User.objects.get(username=request.POST.get("selected_id"))
-    User.delete(user)
-    return render(request, 'successful_action.html', {'result': "User deleted successfully."})
-
-def approve_doggiesitter(request):
-    user = User.objects.get(username=request.POST.get("selected_id"))
-    for i in Accounts.objects.all():
-        if str(i) == str(user):
-            ac = Accounts.objects.get(id=i.id)
-    ac.approved = True
-    user.save()
-    ac.save()
-    return render(request, 'successful_action.html', {'result': "Doggiesitter was successfully approved."})
+# admin.site.register(Accounts)
 
 
-class NewAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'is_doggiesitter', 'approved']
-    ordering = ['first_name', 'last_name']
-    actions = [make_new_admin, delete_admin]
+# @admin.action(description='Grant admin permissions')
+# def make_new_admin(request):
+#     user = User.objects.get(username=request.POST.get("selected_id"))
+#     for i in Accounts.objects.all():
+#         if str(i) == str(user):
+#             ac = Accounts.objects.get(id=i.id)
+#     ac.is_admin = True
+#     user.is_superuser = True
+#     user.is_staff = True
+#     user.save()
+#     ac.save()
+#     return render(request, 'successful_action.html', {'result': "Admin permissions was successfully granted."})
+
+# @admin.action(description='Disable admin permissions')
+# def delete_admin(request):
+#     user = User.objects.get(username=request.POST.get("selected_id"))
+#     for i in Accounts.objects.all():
+#         if str(i) == str(user):
+#             ac = Accounts.objects.get(id=i.id)
+#     ac.is_admin = False
+#     user.is_superuser = False
+#     user.is_staff = False
+#     user.save()
+#     ac.save()
+#     return render(request, 'successful_action.html', {'result': "Admin permissions was successfully denied."})
+
+# def delete_user(request):
+#     user = User.objects.get(username=request.POST.get("selected_id"))
+#     User.delete(user)
+#     return render(request, 'successful_action.html', {'result': "User deleted successfully."})
+
+# def approve_doggiesitter(request):
+#     user = User.objects.get(username=request.POST.get("selected_id"))
+#     for i in Accounts.objects.all():
+#         if str(i) == str(user):
+#             ac = Accounts.objects.get(id=i.id)
+#     ac.approved = True
+#     user.save()
+#     ac.save()
+#     return render(request, 'successful_action.html', {'result': "Doggiesitter was successfully approved."})
 
 
-admin.site.register(Accounts, NewAdmin)
+# class NewAdmin(admin.ModelAdmin):
+#     list_display = ['first_name', 'last_name', 'is_doggiesitter', 'approved']
+#     ordering = ['first_name', 'last_name']
+#     actions = [make_new_admin, delete_admin]
+
+
+# admin.site.register(Accounts, NewAdmin)
 
