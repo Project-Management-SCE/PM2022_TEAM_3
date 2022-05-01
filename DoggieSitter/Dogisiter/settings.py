c266101c653517e8dc3ec7209c77d801166a4774
BoazBitt <73490112+BoazBitt@users.noreply.github.com>
2022-05-01 18:49:59 +0300

    update files

--- DoggieSitter/Dogisiter/settings.py
+++ DoggieSitter/Dogisiter/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'django_jenkins'
 
 ]
 JENKINS_TASKS = (
@@ -86,11 +85,11 @@ WSGI_APPLICATION = 'Dogisiter.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+x = str(BASE_DIR / "db.sqlite3")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': x,
     }
 }
 
